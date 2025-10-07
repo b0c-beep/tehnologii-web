6022942f58b1dd012d359b96b459818ee13558a4
b0c-beep <bocmarioadrian98@gmail.com>
2025-10-07 11:58:17 +0300

    inceput seminar2

--- sem1/express_server.js
+++ sem1/express_server.js
@@ -3,4 +3,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(port);
+app.use(express.static('./sem1/src/public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.get('/server/ping', (req, res) => {
+    res.send('Pong');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Express server running at http://localhost:${port}/`);
+});
