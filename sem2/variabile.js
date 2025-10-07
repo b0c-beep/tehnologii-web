9af7287aa69bf2c78c60597715b408906c5b38c5
b0c-beep <bocmarioadrian98@gmail.com>
2025-10-07 12:11:12 +0300

    new file

---
+++ sem2/variabile.js
@@ -0,0 +1,16 @@
+var nr1 = 10;
+console.log(nr1);
+
+var nr1 = 20;
+console.log(nr1);
+
+let nr2 = 100;
+console.log(nr2);
+//let nr2 = 200; // eroare
+//console.log(nr2);
+
+
+const PI = 3.14;
+console.log(PI);
+//PI = 3.1415; // eroare
+//console.log(PI);
